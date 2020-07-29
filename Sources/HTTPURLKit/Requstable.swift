b59cb92acdfbb5ed14bfcf72ab272167be4a5225
Jaehong Kang <sinoru@me.com>
2020-07-29 15:52:39 +0900

    Make it @_exported

--- Sources/HTTPURLKit/Requstable.swift
+++ Sources/HTTPURLKit/Requstable.swift
@@ -2,6 +2,9 @@ import Foundation
 import URLKit
 import Alamofire
 
+@_exported import struct URLKit.EmptyParameters
+@_exported import struct URLKit.EmptyResponse
+
 public protocol Requestable: URLKit.Requestable {
     var httpMethod: URLRequest.HTTPMethod { get }
     var httpHeaders: [URLRequest.HTTPHeaderFieldName: String]? { get }
