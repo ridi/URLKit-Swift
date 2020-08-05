4dbf29d488d7377623ac856db1577b05a0e3d2c2
Jaehong Kang <sinoru@me.com>
2020-08-05 11:46:46 +0900

    Move public modifier to extension

--- Sources/RxHTTPURLKit/Session+Rx.swift
+++ Sources/RxHTTPURLKit/Session+Rx.swift
@@ -3,9 +3,9 @@ import RxSwift
 
 extension Session: ReactiveCompatible {}
 
-extension Reactive where Base: Session {
+public extension Reactive where Base: Session {
     @discardableResult
-    public func request<T: Requestable>(
+    func request<T: Requestable>(
         _ request: T
     ) -> Single<Response<T.ResponseBody, Error>> {
         Single.create { single in
